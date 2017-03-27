03bbcdb84620a326349b4cf14643864804b3d04b
Enrique Enciso <encisoenrique@gmail.com>
2017-03-27 09:56:39 -0600

    added candidates to election

--- candidates.js
+++ candidates.js
@@ -1,21 +1,39 @@
 var candidates = [
   {
-    user_id: '20167623',
-    screen_name: '',
-    political_party:'PPP',
-    political_orientation: '@kdnuggets'
+    user_id: '41421105',
+    screen_name: '@JosefinaVM',
+    political_party:'PAN',
+    political_orientation: 'Derecha católica'
   },
   {
-    user_id: '16024029',
-    screen_name: '',
-    political_party:'PPP',
-    political_orientation: '@3scale'
+    user_id: '3166004946',
+    screen_name: '@delfinagomeza',
+    political_party:'Morena',
+    political_orientation: 'Izquierda'
   },
   {
-    user_id: '123597227',
-    screen_name: '',
-    political_party:'PPP',
-    political_orientation: '@eenciso_'
+    user_id: '39860797',
+    screen_name: '@alfredodelmazo',
+    political_party:'PRI',
+    political_orientation: 'Centro'
+  },
+  {
+    user_id: '407091449',
+    screen_name: '@IsidroPastor_',
+    political_party:'Independiente',
+    political_orientation: 'Desconocida'
+  },
+  {
+    user_id: '198668037',
+    screen_name: '@OscarGonzalezYa',
+    political_party:'PT',
+    political_orientation: 'Izquierda'
+  },
+  {
+    user_id: '583488712',
+    screen_name: '@JuanZepeda_',
+    political_party:'PRD',
+    political_orientation: 'Izquierda'
   }
 
 ];
